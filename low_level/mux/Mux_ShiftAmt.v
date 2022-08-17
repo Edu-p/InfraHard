d397f388ac82cc137857b74d828c1f4d840a02f1
Arthur Scanoni <61999297+Arthursca@users.noreply.github.com>
2022-08-17 14:53:15 -0300

    feat: Mux_ShiftAmt.v

---
+++ low_level/mux/Mux_ShiftAmt.v
@@ -0,0 +1,11 @@
+module Muu_ShiftAmt(
+    input wire [4:0] in0,
+    input wire  [4:0] in1,
+    input wire [1:0] control,
+    output wire [4:0] out
+    );
+
+    assign out = (control == 2'b00) ? in0:
+                 in1;
+                 
+endmodule
