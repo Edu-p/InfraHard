e69ea4362e3f0a31b072f645a527b24d087943ab
Arthur Scanoni <arthur.scanoni@hotmail.com>
2022-08-23 14:22:32 -0300

    fix: adjust the size of the binary number

--- low_level/mux/Mux_PcSource.v
+++ low_level/mux/Mux_PcSource.v
@@ -5,15 +5,15 @@ module Mux_PcSource(
     input wire  [31:0] in3,
     input wire  [31:0] in4,
     input wire  [31:0] in5,
-    input wire  [1:0] control,
+    input wire  [2:0] control,
     output wire [31:0] out
     );
 
-    assign out = (control == 2'b000) ? in0:
-                 (control == 2'b001) ? in1:
-                 (control == 2'b010) ? in2:
-                 (control == 2'b011) ? in3:
-                 (control == 2'b100) ? in4:
+    assign out = (control == 3'b000) ? in0:
+                 (control == 3'b001) ? in1:
+                 (control == 3'b010) ? in2:
+                 (control == 3'b011) ? in3:
+                 (control == 3'b100) ? in4:
                  in5;
                  
 endmodule
