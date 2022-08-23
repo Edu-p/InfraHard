77a048b580f3ee5defa2ba4acf6d75f0256748cd
Arthur Scanoni <arthur.scanoni@hotmail.com>
2022-08-23 14:22:57 -0300

    fix: adjust the size of the binary number

--- low_level/mux/Mux_SrcData.v
+++ low_level/mux/Mux_SrcData.v
@@ -8,18 +8,18 @@ module Mux_SrcData(
     input wire  [31:0] in6,
     input wire  [31:0] in7,
     input wire  [31:0] in8,
-    input wire [1:0] control,
+    input wire [3:0] control,
     output wire [31:0] out
     );
 
-    assign out = (control == 2'b0000) ? in0:
-                 (control == 2'b0001) ? in1:
-                 (control == 2'b0010) ? in2:
-                 (control == 2'b0011) ? in3:
-                 (control == 2'b0100) ? in4:
-                 (control == 2'b0101) ? in5:
-                 (control == 2'b0110) ? in6:
-                 (control == 2'b0111) ? in7:
+    assign out = (control == 4'b0000) ? in0:
+                 (control == 4'b0001) ? in1:
+                 (control == 4'b0010) ? in2:
+                 (control == 4'b0011) ? in3:
+                 (control == 4'b0100) ? in4:
+                 (control == 4'b0101) ? in5:
+                 (control == 4'b0110) ? in6:
+                 (control == 4'b0111) ? in7:
                  in8;
                  
 endmodule
