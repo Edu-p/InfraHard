1064f8563383c2b979a9b062daeb8199c0d7382e
Arthur Scanoni <61999297+Arthursca@users.noreply.github.com>
2022-08-25 14:17:27 -0300

    refactor: ExtendShiftLeft2
    change input format

--- low_level/ExtendShiftLeft2.v
+++ low_level/ExtendShiftLeft2.v
@@ -1,8 +1,10 @@
 module ExtendShiftLeft2(
-    input wire [25:0] in,
+    input wire [4:0] in1,
+    input wire [4:0] in2,
+    input wire [15:0] in3,
     output wire [27:0] out
 );
-    assign out = {{2'b00}, in};
+    assign out = {{2'b00},in1,in2 ,in3};
     assign out = out << 2; 
 
 endmodule
