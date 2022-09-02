58c5aad6c9659c6683f6f6d3fefd060756399ebe
Arthur Scanoni <arthur.scanoni@hotmail.com>
2022-09-01 23:17:00 -0300

    feat: Control
    SLLV

--- Control.v
+++ Control.v
@@ -354,7 +354,25 @@ module Control(
                                 end
                             end 
 
-
+                            SLLV:begin
+                               shiftSrc = 1'b0;
+                               shiftAmt = 1'b0;
+                               if(counter == 6'b000000 | counter == 6'b000001)begin
+                                    shiftControl = 3'b001;
+                                    counter = counter + 1;
+                                end
+                                else if(counter == 6'b000010) begin
+                                    shiftControl = 3'b010;
+                                    counter = counter + 1; 
+                                end
+                                else begin
+                                    srcData = 4'b0111;
+                                    srcWrite = 3'b001;
+                                    regWrite = 1'b1;
+                                    state = fetch;
+                                    counter = 6'b000000;
+                                end
+                            end 
 
                             JR: begin
                                 aluSrcA = 2'b01;
